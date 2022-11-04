66b8bd34424efc34c3ceedcca88d08b740289ab9
김현진 <kimhyeonjin@gimhyeonjin-ui-MacBookPro.local>
2022-11-04 12:45:19 +0900

    test commit

---
+++ src/main/java/com/umc3/umc3_demo/Umc3DemoApplication.java
@@ -0,0 +1,13 @@
+package com.umc3.umc3_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc3DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Umc3DemoApplication.class, args);
+    }
+
+}
